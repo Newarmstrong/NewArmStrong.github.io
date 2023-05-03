c7d489831ae39e322c7e8c5cd9768178fa7a108a
emanuel-alves <emanuel.alves@unifei.edu.br>
2023-05-03 01:25:31 -0300

    Atualização 2023 V1.0.2

---
+++ replace.ps1
@@ -0,0 +1,9 @@
+$files = Get-ChildItem -File -Filter "*.html"
+
+echo "end"
+foreach ($file in $files) {
+    
+    cat $file.Name
+    (Get-Content $file.Name).Replace("Made with","") | Set-Content $file.Name
+    (Get-Content $file.Name).Replace("Created with","") | Set-Content $file.Name
+}
